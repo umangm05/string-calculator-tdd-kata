cf57add9a2d8d8b4846f537da9d62edec2012bd8
umangm05 <uagarwal303@gmail.com>
2025-10-01 13:21:20 +0530

    Added a dummy function for add function implementation + installed ts-node

---
+++ src/add.test.ts
@@ -0,0 +1,7 @@
+import { add } from './add'
+
+describe('add', () => {
+  it('should be a function', () => {
+    expect(typeof add).toBe('function')
+  })
+})
---
+++ src/add.ts
@@ -0,0 +1,4 @@
+export function add(numbers: string): number {
+  // Implementation will follow TDD; return placeholder for now
+  return 0
+}
